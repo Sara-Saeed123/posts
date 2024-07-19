fdfa8babea543f5246e2d2789b563bdc9b06dd11
Sara-Saeed123 <sara12.saeed123@gmail.com>
2024-07-20 01:18:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(reply => reply.json())
+.then(posts => posts.forEach(data => {
+    var apiContainer = document.getElementById('api');
+    var post = document.createElement('div');
+    post.classList.add('post' , 'col-md-6')
+    post.innerHTML = `
+    <h2>${data.title}</h2>
+    <p>${data.body}</p>
+
+    `;
+ 
+    apiContainer.appendChild(post);
+
+    
+
+    
+})
+)
